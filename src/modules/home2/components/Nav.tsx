35d2c62670c92b755f159d3c18b4c67ae8cc444a
HaNhatPhuong <hnphuong216@gmail.com>
2024-07-17 12:54:31 +0800

    update

--- src/modules/home2/components/Nav.tsx
+++ src/modules/home2/components/Nav.tsx
@@ -28,17 +28,21 @@ function Nav() {
 
   return (
     <div className="bg-[#001635]">
-      <div className=" h-[52px] mx-[174px] flex items-center  px-4 text-white text-sm ">
-        <p className="mr-4">We are leading consultant firm!</p>
-        <div className="flex items-center ">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_Kingdom.svg"
-            alt="UK Flag"
-            className="w-6 h-4"
-          />
-          <span>English</span>
+      <div className=" h-[52px] mx-[174px] flex items-center justify-between px-4 text-white text-sm ">
+        <div className="flex">
+          <p className="mr-4">We are leading consultant firm!</p>
+          <div className="">
+            <div className="flex items-center ">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_Kingdom.svg"
+                alt="UK Flag"
+                className="w-6 h-4"
+              />
+              <span>English</span>
+            </div>
+          </div>
         </div>
-        <div className="flex gap-4 mr-14">
+        <div className="flex gap-4 ">
           <a href="">
             <ImFacebook size={20} />
           </a>
@@ -53,97 +57,99 @@ function Nav() {
           </a>
         </div>
       </div>
-      <div
-        className={`${
-          isNavFixed
-            ? "fixed top-0 w-full z-50 bg-white transition-transform duration-500 ease-in-out transform translate-y-0"
-            : "relative transition-transform duration-500 ease-in-out transform"
-        } h-[77px] bg-white flex items-center pl-[55px]`}
-      >
-        <img
-          src="https://html.creativegigstf.com/charles/images/logo/logo.png"
-          alt="Charles Logo"
-        />
-        <div className="flex mr-auto">
-          <ul className="text-[#001635] flex pl-[35px] items-center space-x-6">
-            {[
-              {
-                name: "HOME",
-                links: [
-                  { name: "Home version one", path: "/" },
-                  { name: "Home version two", path: "/home2" },
-                ],
-              },
-              {
-                name: "PAGES",
-                links: [
-                  { name: "ABOUT US", path: "/aboutus" },
-                  { name: "OUR TEAM", path: "/ourteam" },
-                  { name: "FAQ'S", path: "/fqa" },
-                  { name: "404", path: "/404" },
-                  { name: "SHOP", path: "/shop" },
-                  { name: "SHOP DETAILS", path: "/shopdetails" },
-                ],
-              },
-              {
-                name: "SERVICE",
-                links: [
-                  { name: "SERVICE ONE", path: "/serviceone" },
-                  { name: "SERVICE TWO", path: "/servicetwo" },
-                  { name: "DETAILS SERVICE", path: "/servicedtails" },
-                ],
-              },
-              {
-                name: "PORTFOLIO",
-                links: [
-                  { name: "PROJECT", path: "/project" },
-                  { name: "PROJECT DETAILS", path: "/projectdetails" },
-                ],
-              },
-              {
-                name: "BLOGS",
-                links: [
-                  { name: "BLOG LIST", path: "/bloglist" },
-                  { name: "BLOG GIRD", path: "/bloggird" },
-                  { name: "BLOG DETAILS", path: "/blogdetails" },
-                ],
-              },
-              { name: "CONTACT", links: [] },
-            ].map((item, index) => (
-              <li
-                key={index}
-                className="relative group pr-[40px] flex items-center"
-              >
-                <a
-                  href="#"
-                  className="flex items-center hover:text-[#001635] space-x-1"
+      <div>
+        <div
+          className={`${
+            isNavFixed
+              ? "fixed top-0 w-full z-50 bg-white transition-transform duration-500 ease-in-out transform translate-y-0"
+              : "relative transition-transform duration-500 ease-in-out transform"
+          } h-[77px] bg-white flex items-center pl-[55px]`}
+        >
+          <img
+            src="https://html.creativegigstf.com/charles/images/logo/logo.png"
+            alt="Charles Logo"
+          />
+          <div className="flex mr-auto">
+            <ul className="text-[#001635] flex pl-[35px] items-center space-x-6">
+              {[
+                {
+                  name: "HOME",
+                  links: [
+                    { name: "Home version one", path: "/" },
+                    { name: "Home version two", path: "/home2" },
+                  ],
+                },
+                {
+                  name: "PAGES",
+                  links: [
+                    { name: "ABOUT US", path: "/aboutus" },
+                    { name: "OUR TEAM", path: "/ourteam" },
+                    { name: "FAQ'S", path: "/fqa" },
+                    { name: "404", path: "/404" },
+                    { name: "SHOP", path: "/shop" },
+                    { name: "SHOP DETAILS", path: "/shopdetails" },
+                  ],
+                },
+                {
+                  name: "SERVICE",
+                  links: [
+                    { name: "SERVICE ONE", path: "/serviceone" },
+                    { name: "SERVICE TWO", path: "/servicetwo" },
+                    { name: "DETAILS SERVICE", path: "/servicedtails" },
+                  ],
+                },
+                {
+                  name: "PORTFOLIO",
+                  links: [
+                    { name: "PROJECT", path: "/project" },
+                    { name: "PROJECT DETAILS", path: "/projectdetails" },
+                  ],
+                },
+                {
+                  name: "BLOGS",
+                  links: [
+                    { name: "BLOG LIST", path: "/bloglist" },
+                    { name: "BLOG GIRD", path: "/bloggird" },
+                    { name: "BLOG DETAILS", path: "/blogdetails" },
+                  ],
+                },
+                { name: "CONTACT", links: [] },
+              ].map((item, index) => (
+                <li
+                  key={index}
+                  className="relative group pr-[40px] flex items-center"
                 >
-                  {item.name} {item.links.length > 0 && <IoIosArrowDown />}
-                </a>
-                {item.links.length > 0 && (
-                  <ul
-                    className="absolute left-0 top-full bg-white text-[#001635] w-[200px] overflow-hidden max-h-0 
-                  transition-all duration-700 ease-out group-hover:max-h-[500px] "
+                  <a
+                    href="#"
+                    className="flex items-center hover:text-[#001635] space-x-1"
                   >
-                    {item.links.map((link, idx) => (
-                      <Link key={idx} to={link.path}>
-                        <a className="block px-4 py-2 hover:bg-slate-800 hover:text-white">
-                          {link.name}
-                        </a>
-                      </Link>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="text-[#001635] flex items-center space-x-4 mr-4">
-          <div className="flex items-center space-x-2">
-            <BsCartCheck size={20} />
-            <span className="text-sm">(2)</span>
+                    {item.name} {item.links.length > 0 && <IoIosArrowDown />}
+                  </a>
+                  {item.links.length > 0 && (
+                    <ul
+                      className="absolute left-0 top-full bg-white text-[#001635] w-[200px] overflow-hidden max-h-0 
+                  transition-all duration-700 ease-out group-hover:max-h-[500px] "
+                    >
+                      {item.links.map((link, idx) => (
+                        <Link key={idx} to={link.path}>
+                          <a className="block px-4 py-2 hover:bg-slate-800 hover:text-white">
+                            {link.name}
+                          </a>
+                        </Link>
+                      ))}
+                    </ul>
+                  )}
+                </li>
+              ))}
+            </ul>
           </div>
-          <AiOutlineSearch size={20} />
+          <div className="text-[#001635] flex items-center space-x-4 mr-4">
+            <div className="flex items-center space-x-2">
+              <BsCartCheck size={20} />
+              <span className="text-sm">(2)</span>
+            </div>
+            <AiOutlineSearch size={20} />
+          </div>
         </div>
       </div>
     </div>
