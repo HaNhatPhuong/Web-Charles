cdc0350403dbabf1fe3ed760c3551c4b034dc153
HaNhatPhuong <hnphuong216@gmail.com>
2024-07-27 05:04:39 +0800

    update

--- src/modules/home2/components/FQA.tsx
+++ src/modules/home2/components/FQA.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 function Whywebest() {
   const Whywebestimg = "/src-img/Home2best.jpg";
-
   const [openTopic, setOpenTopic] = useState(null);
+  const [showVideo, setShowVideo] = useState(false);
 
   const usefulTopics = [
     {
@@ -40,15 +40,14 @@ function Whywebest() {
   return (
     <div>
       <div
-        className="  h-[570px] bg-[#0d1427]  bg-cover bg-center bg-no-repeat bg-fixed "
+        className="h-[570px] bg-[#0d1427] bg-cover bg-center bg-no-repeat bg-fixed"
         style={{
           backgroundImage: `url(${Whywebestimg})`,
           backgroundBlendMode: "overlay",
         }}
       >
         <div className="mx-[174px] pt-[95px]">
-          {/*  */}
-          <div className=" text-center ">
+          <div className="text-center">
             <h1 className="pb-[22px] text-[38px] text-white font-bold">
               FQA'S
               <div className="pt-4">
@@ -56,19 +55,55 @@ function Whywebest() {
               </div>
             </h1>
             <p className="text-[18px] text-white">
-              A tale of a fateful trip thatstarted from this tropic port aboard
+              A tale of a fateful trip that started from this tropic port aboard
               this tiny ship today stillers
             </p>
           </div>
-          {/*  */}
           <div className="flex flex-wrap pt-[55px]">
-            <div className="w-full lg:w-1/2">
-              <img src="/src-img/Whywebestimg2.jpg" alt="" />
+            <div className="w-full lg:w-1/2 relative">
+              {!showVideo ? (
+                <div
+                  className="cursor-pointer"
+                  onClick={() => setShowVideo(true)}
+                >
+                  <img
+                    src="/src-img/Home2video.jpg"
+                    alt="Thumbnail"
+                    className="w-full h-auto"
+                  />
+                  <div className="absolute inset-0 flex items-center justify-center">
+                    <a
+                      href="#"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setShowVideo(true);
+                      }}
+                      className=" w-16 h-16 bg-white rounded-full flex items-center justify-center shadow-lg"
+                    >
+                      <img
+                        className=" rounded-full"
+                        src="/src-img/play2.avif"
+                        alt="Play Button"
+                      />
+                    </a>
+                  </div>
+                </div>
+              ) : (
+                <div className="w-full h-0 pb-[56.25%] relative">
+                  <iframe
+                    className="absolute top-0 left-0 w-full h-full"
+                    src="https://www.youtube.com/embed/iwWtofDtcV4"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="YouTube video"
+                  ></iframe>
+                </div>
+              )}
             </div>
-            {/*  */}
-            <div className="p-4 w-full lg:w-1/2 bg-white text-xl  text-[#555555]">
+            <div className="p-4 w-full lg:w-1/2 bg-white text-xl text-[#555555]">
               {usefulTopics.map((topic, index) => (
-                <div key={index} className="mb-6 ">
+                <div key={index} className="mb-6">
                   <p
                     className="font-semibold cursor-pointer border-b-2 border-slate-200 pb-3"
                     onClick={() => toggleTopic(index)}
@@ -88,7 +123,6 @@ function Whywebest() {
               ))}
             </div>
           </div>
-          {/*  */}
         </div>
       </div>
     </div>
